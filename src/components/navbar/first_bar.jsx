439a0cf831d14d8643cdf19896ae179ddb01bf9c
swetha <swethasambothi@gmail.com>
2022-01-25 01:23:04 +0530

    dd

--- src/components/navbar/first_bar.jsx
+++ src/components/navbar/first_bar.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import {en1} from "../../images/en1.jpg"
 
 const FirstBar = () => {
     return <>
              <div className="first_bar text-center">
                  <ul>
-                     <li><img src={en1} alt="" /></li>
+                     <li></li>
                      <li></li>
                      <li></li>
                      <li></li>
