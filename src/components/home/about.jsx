9630aeba77d92e21f368724009ce01fbaf23327c
rahul <rahulparmar7654321@gmail.com>
2022-01-24 09:01:33 +0530

    commit

--- src/components/home/about.jsx
+++ src/components/home/about.jsx
@@ -7,9 +7,9 @@ const About = () => {
                  <h1 className="font-bold">About PTU</h1>
                  <p>Puducherry Technological University (Erstwhile Pondicherry Engineering College) is an Institution promoted and fully funded by the Government of Puducherry, India. It was established as an Engineering College in the year 1984 under the VII Five Year Plan. The college was upgraded to technological university with the approval of the UGC with effect from 5th September, 2020. The PTU was inaugurated by Shri. Venkaiah Naidu, Hon'ble Vice President, Govt. of India on 13.09.2021. The institution is ranked 144 in Engineering Category by NIRF India Rankings 2021.</p>
                </div>
-              <div className="img-slider bg-yellow-200" style={{width: "1000px"}}>
+              {/* <div className="img-slider bg-yellow-200" style={{width: "1000px"}}>
                       we can include image curosle here
-              </div>
+              </div> */}
           </div>
 
           <div className="m-10">
@@ -19,52 +19,52 @@ const About = () => {
 
           <div className="recruiters m-10">
              <h1 className="font-bold">Top recruiters</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
 
           <div className="m-10">
              <h1 className="font-bold">Partnerships</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
 
           <div className="m-10">
              <h1 className="font-bold">Important events</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
 
           <div className="m-10">
              <h1 className="font-bold">Student life</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
 
           <div className="m-10">
              <h1 className="font-bold">Inovation center</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
 
           <div className="m-10">
              <h1 className="font-bold">Student council</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
 
           <div className="m-10">
              <h1 className="font-bold">Sports</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
 
           <div className="m-10">
              <h1 className="font-bold">various clubs</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
 
           <div className="m-10">
              <h1 className="font-bold">contacts</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
 
           <div className="m-10">
              <h1 className="font-bold">newsletter only for pec mail ids</h1>
-             we can make a slider to show top recruiters
+             {/* we can make a slider to show top recruiters */}
           </div>
           </>
 }
