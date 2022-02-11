a2e3b641720ef58cdb447fef2d4083e3ab853ff6
rahul <rahulparmar7654321@gmail.com>
2022-02-11 08:08:22 +0530

    aa

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import CarouselHold from "./Carousel_hold";
 import StudentCount from "./student_Count/Student_Count";
 import CampusLifeOne from "./campus_life/Campus_life1";
-import AboutComp from "./AboutPTU/About";
+// import About from "./AboutPTU/About";
 import News from "./news";
 import Header from "../header/Header";
 
@@ -12,7 +12,7 @@ export default function Home() {
     return (
         <div>
             <Header/>
-           <AboutComp />
+           {/* <About /> */}
            <News />
            <CarouselHold />
            <StudentCount />
